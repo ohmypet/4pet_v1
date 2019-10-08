f8cd102f40fe91af33f36334479dc638c0cc6418
Vi Chi Thien <meomeocf98@gmail.com>
2019-10-08 22:33:58 +0700

    ignore_config

---
+++ script/ignore_file.sh
@@ -0,0 +1,5 @@
+git update-index --assume-unchanged .\ios\Flutter\flutter_export_environment.sh
+git update-index --assume-unchanged .\android\app\build.gradle
+git update-index --assume-unchanged .\android\app\google-services.json
+git update-index --assume-unchanged .\ios\Runner\GoogleService-Info.plist
+git update-index --assume-unchanged .\t_core\lib\config\config.dart
