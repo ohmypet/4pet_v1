55dc791021eea5d2a2ab6617fec926a2c04b2470
Thien Chi Vi <meomeocf98@gmail.com>
2019-11-16 22:10:29 +0700

    Complete update fonts (#24)

--- lib/common/resources/font_families.dart
+++ lib/common/resources/font_families.dart
@@ -1,9 +1,5 @@
 part of petisland.common.resources;
+
 abstract class FontFamilies {
-  static const String harmonia = "HarmoniaSansProCyr";
-  static const String round = "Round";
-  static const String gothic = "Gothic";
-  static const String graceland = "Graceland";
-  static const String tiempos = "Tiempos";
-  static const String doulosSIL = 'DoulosSIL';
+  static const String sarabun = "Sarabun";
 }
--- lib/common/resources/t_text_styles.dart
+++ lib/common/resources/t_text_styles.dart
@@ -7,65 +7,72 @@ abstract class TTextStyles extends TextStyle {
   /// Font size default is **14**
   ///
   /// **Back** is a color default
-  factory TTextStyles.normal({double fontSize, Color color}) => _TTextStyle(
+  factory TTextStyles.normal({double fontSize, Color color, double letterSpacing}) => _TTextStyle(
         fontSize: fontSize,
         color: color,
+        letterSpacing: letterSpacing,
         fontWeight: FontWeight.normal,
       );
 
   /// Font size default is **14**
   ///
   /// **Back** is a color default
-  factory TTextStyles.light({double fontSize, Color color}) => _TTextStyle(
+  factory TTextStyles.light({double fontSize, Color color, double letterSpacing}) => _TTextStyle(
         fontSize: fontSize,
         color: color,
+        letterSpacing: letterSpacing,
         fontWeight: FontWeight.w400,
       );
 
   /// Font size default is **14**
   ///
   /// **Back** is a color default
-  factory TTextStyles.medium({double fontSize, Color color}) => _TTextStyle(
+  factory TTextStyles.medium({double fontSize, Color color, double letterSpacing}) => _TTextStyle(
         fontSize: fontSize,
         color: color,
+        letterSpacing: letterSpacing,
         fontWeight: FontWeight.w500,
       );
 
   /// Font size default is **14**
   ///
   /// **Back** is a color default
-  factory TTextStyles.semi({double fontSize, Color color}) => _TTextStyle(
+  factory TTextStyles.semi({double fontSize, Color color, double letterSpacing}) => _TTextStyle(
         fontSize: fontSize,
         color: color,
+        letterSpacing: letterSpacing,
         fontWeight: FontWeight.w600,
       );
 
   /// Font size default is **14**
   ///
   /// **Back** is a color default
-  factory TTextStyles.bold({double fontSize, Color color}) => _TTextStyle(
+  factory TTextStyles.bold({double fontSize, Color color, double letterSpacing}) => _TTextStyle(
         fontSize: fontSize,
         color: color,
+        letterSpacing: letterSpacing,
         fontWeight: FontWeight.w800,
       );
 
   /// Font size default is **14**
   ///
   /// **Back** is a color default
-  factory TTextStyles.black({double fontSize, Color color}) => _TTextStyle(
+  factory TTextStyles.black({double fontSize, Color color, double letterSpacing}) => _TTextStyle(
         fontSize: fontSize,
         color: color,
         fontWeight: FontWeight.w900,
+        letterSpacing: letterSpacing,
       );
 }
 
 class _TTextStyle extends TextStyle implements TTextStyles {
-  const _TTextStyle({double fontSize, Color color, FontWeight fontWeight})
+  const _TTextStyle({double fontSize, Color color, FontWeight fontWeight, double letterSpacing})
       : super(
           height: 1.4,
           fontWeight: fontWeight,
           fontSize: fontSize ?? 14,
           color: color ?? TColors.black,
-          fontFamily: FontFamilies.harmonia,
+          fontFamily: FontFamilies.sarabun,
+          letterSpacing: letterSpacing ?? 0,
         );
 }
