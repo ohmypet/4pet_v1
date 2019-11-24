dd925acb45c4453e51c8369b6075564aa4fd3436
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-11-24 19:02:33 +0700

    Add constants for service

---
+++ petisland_core/lib/config/constant.dart
@@ -0,0 +1,31 @@
+part of petisland_core.config;
+
+enum Role {
+  Free,
+  Premium
+}
+
+enum AccountStatus {
+  Active,
+  Deactive
+}
+
+enum PostStatus {
+  Pending,
+  New,
+  Done,
+  Expired
+}
+
+enum PetType {
+  Dog,
+  Cat,
+  Bird,
+  Fish,
+  Snake,
+  Hare,
+  Hamster,
+  Other
+}
+
+const int max_image = 10;
