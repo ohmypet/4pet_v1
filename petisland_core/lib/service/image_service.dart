2ebda2b682a1e1cce90635affdfb78f13f220104
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2019-11-25 22:49:30 +0700

    complete service upload image

--- petisland_core/lib/service/image_service.dart
+++ petisland_core/lib/service/image_service.dart
@@ -1,6 +1,16 @@
 part of petisland_core.service;
 
 abstract class ImageService {
-  Future<PetImage> upload(List<String> paths);
+  Future<List<PetImage>> upload(List<String> paths);
 }
 
+class ImageServiceImpl extends ImageService {
+  final ImageRepository repository;
+
+  ImageServiceImpl(this.repository);
+  
+  @override
+  Future<List<PetImage>> upload(List<String> paths) {
+    return repository.upload(paths);
+  }
+}
