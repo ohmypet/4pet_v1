db8261dba5391cfd11713c395936ddd67de3e144
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2019-11-28 15:31:48 +0700

    Complete test for tag service

---
+++ petisland_core/test/service_test/tag_service_test.dart
@@ -0,0 +1,47 @@
+import 'package:petisland_core/petisland_core.dart';
+import 'package:test/test.dart';
+
+import 'configuration.dart';
+
+void main() async {
+  await setupEnvironment();
+  login();
+
+  final TagService service = DI.get<TagService>(TagService);
+  test("Get tags with title is empty", () async {
+    try {
+      final List<Tag> tags = await service.getTags('');
+      for (Tag item in tags) {
+        Log.info(item.toJson());
+      }
+      expect(tags, isNotNull);
+    } catch (ex) {
+      Log.info(ex);
+      assert(false);
+    }
+  });
+  test("Get tags with title is null", () async {
+    try {
+      final List<Tag> tags = await service.getTags(null);
+      for (Tag item in tags) {
+        Log.info(item.toJson());
+      }
+      expect(tags, isNotNull);
+    } catch (ex) {
+      Log.info(ex);
+      assert(false);
+    }
+  });
+  test("Get tags with title dog", () async {
+    try {
+      final List<Tag> tags = await service.getTags('dog');
+      for (Tag item in tags) {
+        Log.info(item.toJson());
+      }
+      expect(tags, isNotNull);
+    } catch (ex) {
+      Log.info(ex);
+      assert(false);
+    }
+  });
+}
