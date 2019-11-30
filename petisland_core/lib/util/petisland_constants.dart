7cb924b7401999548109020b5e49986ae840a1a3
Thien Chi Vi <meomeocf98@gmail.com>
2019-11-30 22:00:25 +0700

    Complete define category (#72)

--- petisland_core/lib/util/petisland_constants.dart
+++ petisland_core/lib/util/petisland_constants.dart
@@ -4,6 +4,22 @@ import 'package:flutter/foundation.dart';
 
 abstract class PetIslandConstants {
   static const String key_token = 'token';
+
+  static const List<String> categories = <String>[
+    'Trending',
+    'Popularity',
+    'Price higth to low',
+    'Price low to hight',
+    'Pet category',
+  ];
+}
+
+enum CategoryEnum {
+  Trending,
+  Popularity,
+  PriceHighToLow,
+  PriceLowToHight,
+  PetCategory,
 }
 
 enum Role { Free, Premium }
@@ -17,5 +33,7 @@ enum PetType { Dog, Cat, Bird, Fish, Snake, Hare, Hamster, Other }
 const int max_image = 10;
 
 String enumToString(Object object) {
+  if (object is CategoryEnum) return PetIslandConstants.categories[object.index];
+
   return describeEnum(object);
 }
