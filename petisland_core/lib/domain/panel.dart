7647f595eb71c7767e39ac3b170f195fcd2363a8
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2019-12-07 16:07:56 +0700

    Complete post service

--- petisland_core/lib/domain/panel.dart
+++ petisland_core/lib/domain/panel.dart
@@ -2,13 +2,26 @@ part of petisland_core.domain;
 
 class Panel extends Item {
   String type;
-  List<PanelDetail<PostItem>> items;
+  List<PanelDetail> items;
 
   Panel.fromJson(Map<String, dynamic> json) {
     type = json['type'];
+
     final List<Map<String, dynamic>> data =
         json['data']?.cast<Map<String, dynamic>>() ?? const <Map<String, dynamic>>[];
 
-    items = data.map((Map<String, dynamic> item) => PanelDetail<PostItem>.fromJson(item));
+    items = data
+        .map((Map<String, dynamic> item) => PanelDetail.fromJson(item))
+        .where((PanelDetail item) => item.type != null && item.postItem != null)
+        .toList();
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> map = <String, dynamic>{};
+    _addValueToMap('type', type, map);
+    final List<Map<String, dynamic>> dataToJson = items.map((PanelDetail item) => item.toJson()).toList();
+    _addValueToMap('data', dataToJson, map);
+    return map;
   }
 }
