9484f664e1a8aa5d8ee084ed9656b315a2503c3b
Thien Chi Vi <meomeocf98@gmail.com>
2019-12-07 17:33:02 +0700

    Complete render layout post (#90)

--- lib/common/state/state.dart
+++ lib/common/state/state.dart
@@ -1,6 +1,7 @@
 library petisland.common.state;
 
 import 'package:flutter/material.dart';
+import 'package:flutter_template/common/widgets/widgets.dart';
 import 'package:petisland_core/util/log.dart';
 
 part 'petisland_stateful_widget.dart';
---
+++ lib/common/widgets/t_page_route.dart
@@ -0,0 +1,65 @@
+part of petisland.common.widgets;
+
+class TPageRoute<T> extends PageRoute<T> {
+  final WidgetBuilder builder;
+
+  @override
+  final bool maintainState;
+
+  TPageRoute({
+    @required this.builder,
+    RouteSettings settings,
+    this.maintainState = true,
+    bool fullscreenDialog = false,
+  })  : assert(maintainState != null, "maintainState must not null"),
+        super(fullscreenDialog: fullscreenDialog ?? false, settings: settings);
+
+  @override
+  Widget buildPage(
+      BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+    final Widget result = builder(context);
+    return Semantics(
+      scopesRoute: true,
+      explicitChildNodes: true,
+      child: result,
+    );
+  }
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    Log.info("$runtimeType buildTransitions");
+    return ScaleTransition(
+      child: child,
+      scale: Tween<double>(begin: 0.0, end: 1).animate(
+        CurvedAnimation(
+          parent: animation,
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Color get barrierColor => null;
+
+  @override
+  String get barrierLabel => null;
+
+  @override
+  bool canTransitionFrom(TransitionRoute<dynamic> previousRoute) {
+    return previousRoute is TPageRoute || previousRoute is TPageRoute;
+  }
+
+  @override
+  bool canTransitionTo(TransitionRoute<dynamic> nextRoute) {
+    return (nextRoute is TPageRoute && !nextRoute.fullscreenDialog);
+  }
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 250);
+}
