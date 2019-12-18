75bc820545a7396f02fbe1ef9b70040beb6077aa
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2019-12-18 15:03:52 +0700

    Add normalize vietnamese

--- petisland_core/lib/utils/utils.dart
+++ petisland_core/lib/utils/utils.dart
@@ -2,8 +2,13 @@ library petisland_core.utils;
 
 import 'package:ansicolor/ansicolor.dart';
 import 'package:flutter/foundation.dart';
+import 'package:tiengviet/tiengviet.dart';
 import 'package:timeago/timeago.dart' as timeAgo;
 
 part 'log.dart';
+
 part 'petisland_constants.dart';
+
+part 'string.dart';
+
 part 'time.dart';
