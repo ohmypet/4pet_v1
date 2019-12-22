987afc87af28fc5cc3d2c6ba9e0450934d1ce604
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2019-12-22 14:27:31 +0700

    Complete update report

--- petisland_core/lib/service/report_service.dart
+++ petisland_core/lib/service/report_service.dart
@@ -3,6 +3,8 @@ part of petisland_core.service;
 abstract class ReportService {
   Future<Report> report(String postId, String reason, String accountId,
       {String description});
+
+  Future<List<String>> getReason();
 }
 
 class ReportServiceImpl extends ReportService {
@@ -14,5 +16,10 @@ class ReportServiceImpl extends ReportService {
   Future<Report> report(String postId, String reason, String accountId,
       {String description}) {
     return repository.report(postId, reason, accountId, description: description);
+  }
+
+  @override
+  Future<List<String>> getReason() {
+    return repository.getReason();
   }
 }
