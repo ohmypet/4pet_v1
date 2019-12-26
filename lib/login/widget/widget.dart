c866cb927ce8837b61eb9f17ddd8dd2efe34822e
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2019-12-27 00:50:48 +0700

    Complete icon logo person

--- lib/login/widget/widget.dart
+++ lib/login/widget/widget.dart
@@ -2,7 +2,6 @@ library petisland.login.widget;
 
 import 'dart:ui';
 
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/common/common.dart';
 import 'package:flutter_template/common/widgets/widgets.dart';
@@ -10,6 +9,9 @@ import 'package:flutter_template/login/bloc/bloc.dart';
 import 'package:flutter_template/register/register.dart';
 
 part 'loading_widget.dart';
+
 part 'login_detail_widget.dart';
+
 part 'petisland_button_widget.dart';
+
 part 'user_input_widget.dart';
